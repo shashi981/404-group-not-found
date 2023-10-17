3d756d72317cf63d0599db88a15cdd07648cadbc
LouieTang <louietang2013@gmail.com>
2023-10-16 20:53:33 -0700

    working on google sign in, and intent linking.

--- front-end/app/src/main/java/com/example/grocerymanager/HomeActivity.java
+++ front-end/app/src/main/java/com/example/grocerymanager/HomeActivity.java
@@ -2,13 +2,68 @@ package com.example.grocerymanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private Button scanGroceriesButton;
+    private Button manageInventoryButton;
+    private Button consultDietitianButton;
+    private Button suggestedRecipesButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        scanGroceriesButton = findViewById(R.id.scan_groceries_button_home);
+        scanGroceriesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchScannerIntent();
+            }
+        });
+        manageInventoryButton = findViewById(R.id.manage_inventory_button_home);
+        manageInventoryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchInventoryIntent();
+            }
+        });
+
+        consultDietitianButton = findViewById(R.id.consult_dietitian_button_home);
+        consultDietitianButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchChatIntent();
+            }
+        });
+
+        suggestedRecipesButton = findViewById(R.id.suggested_recipes_button_home);
+        suggestedRecipesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchRecipeIntent();
+            }
+        });
+    }
+
+    private void launchScannerIntent() {
+        Intent scannerIntent = new Intent(HomeActivity.this, ScannerActivity.class);
+        startActivity(scannerIntent);
+    }
+    private void launchInventoryIntent() {
+        Intent inventoryIntent = new Intent(HomeActivity.this, InventoryActivity.class);
+        startActivity(inventoryIntent);
+    }
+    private void launchChatIntent() {
+        Intent chatIntent = new Intent(HomeActivity.this, ChatActivity.class);
+        startActivity(chatIntent);
+    }
+    private void launchRecipeIntent() {
+        Intent recipeIntent = new Intent(HomeActivity.this, RecipeActivity.class);
+        startActivity(recipeIntent);
     }
 }
--- front-end/app/src/main/java/com/example/grocerymanager/MainActivity.java
+++ front-end/app/src/main/java/com/example/grocerymanager/MainActivity.java
@@ -2,13 +2,113 @@ package com.example.grocerymanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button signInButton;
+    final static String TAG = "MainActivity"; //identify where log is coming from
+
+    private GoogleSignInClient mGoogleSignInClient;
+    private int RC_SIGN_IN = 1;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        signInButton = findViewById(R.id.sign_in_button_login);
+//        signInButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                launchMainIntent();
+//            }
+//        });
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .requestProfile()
+                .build();
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        signInButton = findViewById(R.id.sign_in_button_login);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signIn();
+            }
+        });
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+            updateUI(account);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+            updateUI(null);
+        }
+    }
+    private void updateUI(GoogleSignInAccount account) {
+        if(account == null){
+            Log.d(TAG, "There is no user signed in!");
+        }
+        else{
+            Log.d(TAG, "Pref Name: " + account.getDisplayName());
+            Log.d(TAG, "Email: " + account.getEmail());
+            Log.d(TAG, "Given Name: " + account.getGivenName());
+            Log.d(TAG, "Family Name: " + account.getFamilyName());
+            Log.d(TAG, "Display URI: " + account.getPhotoUrl());
+
+            launchMainIntent();
+            //Send token to back-end
+            //Move to other activity
+//            account.getIdToken();
+//            launchInfoIntent();
+        }
+    }
+
+    private void launchMainIntent() {
+        Intent detailsIntent = new Intent(MainActivity.this, MainActivity.class);
+        startActivity(detailsIntent);
     }
 }
