3eb31f7eb2d22077b2b2121046750b2a56b3c1d0
LouieTang <louietang2013@gmail.com>
2023-10-23 16:39:45 -0700

    front end functionality done

--- front-end/app/src/main/java/com/example/grocerymanager/ListActivity.java
+++ front-end/app/src/main/java/com/example/grocerymanager/ListActivity.java
@@ -3,15 +3,28 @@ package com.example.grocerymanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageButton;
 
 public class ListActivity extends AppCompatActivity {
 
     final static String TAG = "ListActivity"; //identify where log is coming from
 
+    private ImageButton backIcon;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        backIcon = findViewById(R.id.back_icon_list);
+        backIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                finish();
+
+            }
+        });
     }
 }
--- front-end/app/src/main/java/com/example/grocerymanager/SettingsActivity.java
+++ front-end/app/src/main/java/com/example/grocerymanager/SettingsActivity.java
@@ -3,12 +3,29 @@ package com.example.grocerymanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class SettingsActivity extends AppCompatActivity {
+
+
+    final static String TAG = "SettingsActivity"; //identify where log is coming from
+
+    private ImageButton backIcon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        backIcon = findViewById(R.id.back_icon_settings);
+        backIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                finish();
+
+            }
+        });
     }
 }
