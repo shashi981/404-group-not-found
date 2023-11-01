6618f9348282ea01ce73dcadb984d8a3fa22496a
Shashi Kiran <Shashi090801@gmail.com>
2023-11-01 07:23:32 -0700

    socket manager

--- front-end/app/src/main/java/com/example/grocerymanager/ChatActivity.java
+++ front-end/app/src/main/java/com/example/grocerymanager/ChatActivity.java
@@ -1,35 +1,20 @@
 package com.example.grocerymanager;
 
-import static android.system.Os.socket;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Application;
-import com.google.firebase.FirebaseApp;
-
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.PopupMenu;
-import android.os.Bundle;
-import android.widget.EditText;
-import android.widget.Button;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
+import io.socket.client.IO;
 import io.socket.client.Socket;
 import io.socket.emitter.Emitter;
-import java.net.URISyntaxException;
-import io.socket.client.IO;
-
 
 public class ChatActivity extends AppCompatActivity {
 
-    private Socket socket;
-    private EditText messageInput;
-    private Button sendButton;
 
     final static String TAG = "ChatActivity"; //identify where log is coming from
     private ImageButton chatIcon;
@@ -39,27 +24,12 @@ public class ChatActivity extends AppCompatActivity {
     private ImageButton cartIcon;
     private ImageButton menuIcon;
 
+    private Socket mSocket;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
-
-//        chatIcon = findViewById(R.id.chat_icon_scanner);
-//        chatIcon.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//
-//                ActivityLauncher.launchActivity(ScannerActivity.this, ChatActivity.class);
-//
-//            }
-//        });
-
-
-        try {
-            socket = IO.socket("https://20.104.197.24/");
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-        }
 
         scannerIcon = findViewById(R.id.scan_icon_chat);
         scannerIcon.setOnClickListener(new View.OnClickListener() {
@@ -125,59 +95,52 @@ public class ChatActivity extends AppCompatActivity {
             }
         });
 
-        FirebaseApp.initializeApp(this);
+        mSocket= SocketManager.getInstance().getSocket();
 
-
-        messageInput = findViewById(R.id.inputMessage);
-        sendButton = findViewById(R.id.sendButton);
-
-        sendButton.setOnClickListener(new View.OnClickListener() {
+        // Listener for successful connection
+        mSocket.on(Socket.EVENT_CONNECT, new Emitter.Listener() {
             @Override
-            public void onClick(View v) {
-                String message = messageInput.getText().toString();
-                if (!message.isEmpty()) {
-                    sendMessage(message);
-                    messageInput.setText("");
-                }
+            public void call(Object... args) {
+                // You can log or show a toast here to indicate a successful connection
+                Log.d(TAG, "Socket connected");
             }
         });
 
-        socket.on("chat message", onNewMessage);
-        socket.connect();
+        // Listener for disconnection
+        mSocket.on(Socket.EVENT_DISCONNECT, new Emitter.Listener() {
+            @Override
+            public void call(Object... args) {
+                // Log or show a toast for disconnection
+                Log.d(TAG, "Socket disconnected");
+            }
+        });
 
+        // Listener for connection errors
+        mSocket.on(Socket.EVENT_CONNECT_ERROR, new Emitter.Listener() {
+            @Override
+            public void call(Object... args) {
+                Throwable e = (Throwable) args[0];
+                // Log or show the error
+                Log.e(TAG, "Socket connection error", e);
+            }
+        });
+
+        // Listener for other errors
+        mSocket.on(Socket.EVENT_ERROR, new Emitter.Listener() {
+            @Override
+            public void call(Object... args) {
+                Throwable e = (Throwable) args[0];
+                // Log or show the error
+                Log.e(TAG, "Socket error", e);
+            }
+        });
+
+        mSocket.connect();
     }
-
-    private void sendMessage(String message) {
-        // Send the message to the server
-        JSONObject data = new JSONObject();
-        try {
-            data.put("uid", userId);
-            data.put("did", dieticianId);
-            data.put("text", message);
-            data.put("fromUser", 1); // 1 for user, 0 for dietician
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        socket.emit("chat message", data);
-    }
-
-    private Emitter.Listener onNewMessage = new Emitter.Listener() {
-        @Override
-        public void call(final Object... args) {
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    JSONObject data = (JSONObject) args[0];
-                    // Handle and display the incoming message in your UI
-                }
-            });
-        }
-    };
-
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        // Disconnect from the server when the activity is destroyed
-        socket.disconnect();
+        mSocket.disconnect();
+        mSocket.off();
     }
 }
