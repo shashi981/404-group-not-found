1e1ce2fa82438dfe736a783ee5c47debbdde50dc
LouieTang <louietang2013@gmail.com>
2023-11-03 13:19:31 -0700

    finished recipe implementation

--- front-end/app/src/main/java/com/example/grocerymanager/Recipe.java
+++ front-end/app/src/main/java/com/example/grocerymanager/Recipe.java
@@ -5,21 +5,45 @@ package com.example.grocerymanager;
 import android.util.Pair;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class Recipe {
     private String recipeName;
     private List<Pair<String, String>> ingredients;
     private String instructions;
+    private String youTubeLink;
+    private int RID;
 
-    public Recipe(String recipeName, String instructions) {
+    public Recipe(String recipeName, String instructions, int RID, String youTubeLink) {
         this.recipeName = recipeName;
         this.instructions = instructions;
+        this.youTubeLink = youTubeLink;
+        this.RID = RID;
         ingredients = new ArrayList<>();
     }
 
+    public Recipe(int RID){
+        this.recipeName = null;
+        this.instructions = null;
+        this.youTubeLink = null;
+        this.RID = RID;
+        ingredients = new ArrayList<>();
+    }
+
+    public String getYouTubeLink() {
+        return youTubeLink;
+    }
+
+    public void setYouTubeLink(String youTubeLink) {
+        this.youTubeLink = youTubeLink;
+    }
+
+    public void setRecipeName(String recipeName){
+        this.recipeName = recipeName;
+    }
+    public void setInstructions(String instructions){
+        this.instructions = instructions;
+    }
     public String getRecipeName() {
         return recipeName;
     }
@@ -39,5 +63,20 @@ public class Recipe {
             ingredientsString = ingredientsString + ingredientPair.first + ": " + ingredientPair.second + "\n";
         }
         return ingredientsString;
+    }
+    public String getFiveIngredientsAstString(){
+        String ingredientsString = new String();
+        int i = 0;
+        for(Pair<String, String> ingredientPair : ingredients){
+            if(i > 4){
+                break;
+            }
+            ingredientsString = ingredientsString + ingredientPair.first + ": " + ingredientPair.second + "\n";
+            i++;
+        }
+        return ingredientsString;
+    }
+    public int getRID(){
+        return RID;
     }
 }
